ad9a1a7d0df48a47394ff3d036ba6106dad7ebaa
Prabhat <prabhat@mimosa.co>
2017-02-14 01:57:41 -0800

    Testing GIT with pycharm
    Author:    Prabhat <aol.prabhat@gmail.com>

--- myCode.py
+++ myCode.py
@@ -8,7 +8,14 @@ def myFunc():
 
     print 'This is a function call'
 
+    goodFunction ('Input Parameter1', 'Input Parameter2')
+
     return ('Test')
+
+def goodFunction(param1, param2):
+    print 'Call this function from other function'
+    print 'param1'
+    print 'param2'
 
 if __name__ == '__main__':
     myFunc()
